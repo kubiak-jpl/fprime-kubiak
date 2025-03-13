bc4c324cb4857f2e3fda204226906c3aad5abd83
Gerik Kubiak <gerik.kubiak@jpl.nasa.gov>
2025-03-13 12:05:16 -0700

    Address analyzer warnings

--- Svc/OsTime/OsTime.cpp
+++ Svc/OsTime/OsTime.cpp
@@ -28,9 +28,9 @@ OsTime ::~OsTime()
 
 void OsTime::set_epoch(const Fw::Time& fw_time, const Os::RawTime& os_time) {
     Os::ScopeLock lock(m_epoch_lock);
-    m_epoch_fw_time = fw_time;
-    m_epoch_os_time = os_time;
-    m_epoch_valid = true;
+    (void) m_epoch_fw_time = fw_time;
+    (void) m_epoch_os_time = os_time;
+    (void) m_epoch_valid = true;
 }
 
 // ----------------------------------------------------------------------
