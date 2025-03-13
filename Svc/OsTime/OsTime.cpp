76e6fabc549e78cbbeba5bb2937a8450c5b14b38
Gerik Kubiak <gerik.kubiak@jpl.nasa.gov>
2025-03-13 12:21:07 -0700

    Remove use of void

--- Svc/OsTime/OsTime.cpp
+++ Svc/OsTime/OsTime.cpp
@@ -28,9 +28,9 @@ OsTime ::~OsTime()
 
 void OsTime::set_epoch(const Fw::Time& fw_time, const Os::RawTime& os_time) {
     Os::ScopeLock lock(m_epoch_lock);
-    (void) m_epoch_fw_time = fw_time;
-    (void) m_epoch_os_time = os_time;
-    (void) m_epoch_valid = true;
+    m_epoch_fw_time = fw_time;
+    m_epoch_os_time = os_time;
+    m_epoch_valid = true;
 }
 
 // ----------------------------------------------------------------------
@@ -57,7 +57,7 @@ void OsTime ::timeGetPort_handler(FwIndexType portNum, Fw::Time& time) {
         return;
     }
 
-    (void) time = m_epoch_fw_time;
+    time = m_epoch_fw_time;
     time.add(elapsed.getSeconds(), elapsed.getUSeconds());
 }
 
