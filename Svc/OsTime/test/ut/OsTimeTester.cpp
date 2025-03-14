330b31bd124ddf03eb2423c008604c5866065edc
Gerik Kubiak <gerik.kubiak@jpl.nasa.gov>
2025-03-14 08:41:25 -0700

    Update OsTime tests to use RawTimeTester

--- Svc/OsTime/test/ut/OsTimeTester.cpp
+++ Svc/OsTime/test/ut/OsTimeTester.cpp
@@ -6,6 +6,7 @@
 
 #include "OsTimeTester.hpp"
 #include "Svc/OsTime/OsTime.hpp"
+#include "Svc/OsTime/test/RawTimeTester/RawTimeTester.hpp"
 
 #include <cstdint>
 #include <chrono>
@@ -50,75 +51,64 @@ namespace Svc {
     zeroEpochTest()
   {
       Os::RawTime epoch_os_time;
+      Svc::RawTimeTester::setNowTime(Fw::Time(0, 0));
       Os::RawTime::Status os_stat = epoch_os_time.now();
       ASSERT_EQ(os_stat, Os::RawTime::OP_OK);
       component.set_epoch(Fw::ZERO_TIME, epoch_os_time);
 
-      const std::chrono::milliseconds dur_200ms(200);
-
       // 200 ms
-      std::this_thread::sleep_for(dur_200ms);
+      Svc::RawTimeTester::setNowTime(Fw::Time(0, 200*1000));
       Fw::Time time_200ms;
       invoke_to_timeGetPort(0, time_200ms);
-      ASSERT_NE(time_200ms, Fw::ZERO_TIME);
-
-      const double time_200 = time_200ms.getSeconds() + (time_200ms.getUSeconds() / (1000.*1000.));
-      ASSERT_TIMING(0, 0.2, time_200);
+      ASSERT_EQ(time_200ms, Fw::Time(0, 200*1000));
 
       // 400 ms
-      std::this_thread::sleep_for(dur_200ms);
+      Svc::RawTimeTester::setNowTime(Fw::Time(0, 400*1000));
       Fw::Time time_400ms;
       invoke_to_timeGetPort(0, time_400ms);
-      ASSERT_NE(time_400ms, Fw::ZERO_TIME);
+      ASSERT_EQ(time_400ms, Fw::Time(0, 400*1000));
 
-      const double time_400 = time_400ms.getSeconds() + (time_400ms.getUSeconds() / (1000.*1000.));
-      ASSERT_TIMING(0, 0.4, time_400);
+      // 1200 ms
+      Svc::RawTimeTester::setNowTime(Fw::Time(1, 200*1000));
+      Fw::Time time_1200ms;
+      invoke_to_timeGetPort(0, time_1200ms);
+      ASSERT_EQ(time_1200ms, Fw::Time(1, 200*1000));
 
-      // 600 ms
-      std::this_thread::sleep_for(dur_200ms);
-      Fw::Time time_600ms;
-      invoke_to_timeGetPort(0, time_600ms);
-      ASSERT_NE(time_600ms, Fw::ZERO_TIME);
-
-      const double time_600 = time_600ms.getSeconds() + (time_600ms.getUSeconds() / (1000.*1000.));
-      ASSERT_TIMING(0, 0.6, time_600);
-
+      // 123456.987654 s
+      Svc::RawTimeTester::setNowTime(Fw::Time(123456, 987654));
+      Fw::Time time_123456;
+      invoke_to_timeGetPort(0, time_123456);
+      ASSERT_EQ(time_123456, Fw::Time(123456, 987654));
   }
 
   void OsTimeTester ::
     offsetEpochTest()
   {
       Os::RawTime epoch_os_time;
+      Svc::RawTimeTester::setNowTime(Fw::Time(0, 0));
       Os::RawTime::Status os_stat = epoch_os_time.now();
-      Fw::Time start_time(12345, 800000);
-      double start_time_d = 12345.8;
+      Fw::Time start_time(12345, 800*1000);
       ASSERT_EQ(os_stat, Os::RawTime::OP_OK);
       component.set_epoch(start_time, epoch_os_time);
 
-      const std::chrono::milliseconds dur_200ms(200);
-
       // 200 ms
-      std::this_thread::sleep_for(dur_200ms);
+      Svc::RawTimeTester::setNowTime(Fw::Time(0, 200*1000));
       Fw::Time time_200ms;
       invoke_to_timeGetPort(0, time_200ms);
-      ASSERT_NE(time_200ms, Fw::ZERO_TIME);
-
-      const double time_200 = time_200ms.getSeconds() + (time_200ms.getUSeconds() / (1000.*1000.));
-      ASSERT_TIMING(start_time_d, 0.2, time_200);
+      ASSERT_EQ(time_200ms, Fw::Time(12346, 0));
   }
 
   void OsTimeTester ::
     noEpochTest()
   {
+      Svc::RawTimeTester::setNowTime(Fw::Time(0, 0));
       // 0 ms
       Fw::Time time_0ms;
       invoke_to_timeGetPort(0, time_0ms);
       ASSERT_EQ(time_0ms, Fw::ZERO_TIME);
 
-      const std::chrono::milliseconds dur_200ms(200);
-
       // 200 ms
-      std::this_thread::sleep_for(dur_200ms);
+      Svc::RawTimeTester::setNowTime(Fw::Time(0, 200*1000));
       Fw::Time time_200ms;
       invoke_to_timeGetPort(0, time_200ms);
       ASSERT_EQ(time_200ms, Fw::ZERO_TIME);
@@ -128,29 +118,16 @@ namespace Svc {
     updateEpochTest()
   {
       Os::RawTime epoch_os_time;
+      Svc::RawTimeTester::setNowTime(Fw::Time(0, 0));
       Os::RawTime::Status os_stat = epoch_os_time.now();
       ASSERT_EQ(os_stat, Os::RawTime::OP_OK);
       component.set_epoch(Fw::ZERO_TIME, epoch_os_time);
 
-      const std::chrono::milliseconds dur_200ms(200);
-
       // 200 ms
-      std::this_thread::sleep_for(dur_200ms);
+      Svc::RawTimeTester::setNowTime(Fw::Time(0, 200*1000));
       Fw::Time time_200ms;
       invoke_to_timeGetPort(0, time_200ms);
-      ASSERT_NE(time_200ms, Fw::ZERO_TIME);
-
-      const double time_200 = time_200ms.getSeconds() + (time_200ms.getUSeconds() / (1000.*1000.));
-      ASSERT_TIMING(0, 0.2, time_200);
-
-      // 400 ms
-      std::this_thread::sleep_for(dur_200ms);
-      Fw::Time time_400ms;
-      invoke_to_timeGetPort(0, time_400ms);
-      ASSERT_NE(time_400ms, Fw::ZERO_TIME);
-
-      const double time_400 = time_400ms.getSeconds() + (time_400ms.getUSeconds() / (1000.*1000.));
-      ASSERT_TIMING(0, 0.4, time_400);
+      ASSERT_EQ(time_200ms, Fw::Time(0, 200*1000));
 
       // Change time base
       const Fw::Time new_base(TB_WORKSTATION_TIME, 7, 1234, 0);
@@ -159,30 +136,15 @@ namespace Svc {
       invoke_to_setEpoch(0, new_base, epoch_os_time);
 
       // Immediately get time
-      Fw::Time time_400ms_2;
-      invoke_to_timeGetPort(0, time_400ms_2);
-      ASSERT_NE(time_400ms_2, Fw::ZERO_TIME);
-      ASSERT_EQ(time_400ms_2.getTimeBase(), TB_WORKSTATION_TIME);
-      ASSERT_EQ(time_400ms_2.getContext(), 7);
-
-      const double time_400_2 = time_400ms_2.getSeconds() + (time_400ms_2.getUSeconds() / (1000.*1000.));
-      //ASSERT_TIMING(1234.0, 0, time_400_2);
-      ASSERT_LT(1234.0, time_400_2);
-      ASSERT_GT(1234.0 + 0.1, time_400_2);
+      Fw::Time time_200ms_2;
+      invoke_to_timeGetPort(0, time_200ms_2);
+      ASSERT_EQ(time_200ms_2, Fw::Time(TB_WORKSTATION_TIME, 7, 1234, 0));
 
       // 600 ms
-      std::this_thread::sleep_for(dur_200ms);
       Fw::Time time_600ms;
+      Svc::RawTimeTester::setNowTime(Fw::Time(0, 600*1000));
       invoke_to_timeGetPort(0, time_600ms);
-      ASSERT_NE(time_600ms, Fw::ZERO_TIME);
-      ASSERT_EQ(time_600ms.getTimeBase(), TB_WORKSTATION_TIME);
-      ASSERT_EQ(time_600ms.getContext(), 7);
-
-      const double time_600 = time_600ms.getSeconds() + (time_600ms.getUSeconds() / (1000.*1000.));
-      ASSERT_TIMING(1234.0, 0.2, time_600);
-
-
-
+      ASSERT_EQ(time_600ms, Fw::Time(TB_WORKSTATION_TIME, 7, 1234, 400*1000));
   }
 
 }
